dfb9175b45c1c3ee2ee8c5223f8a40534aabfed8
jamooooong <102500033+jamooooong@users.noreply.github.com>
2024-09-06 16:38:52 +0900

    feat: modal 구현을 위한 임시 버튼 구현 (#14)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <div>
+      <Link href="/myPageSetting">Open modal</Link>
+      <Link href="/myPageSettingSchool">Open modal</Link>
       <h2>홈페이지</h2>
     </div>
   );
